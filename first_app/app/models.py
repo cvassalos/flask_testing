bd832d9d861be18a92025e94cd60b5da33c7f70c
cvassalos <vassalce@gmail.com>
2025-11-09 14:00:11 -0500

    Started User class
    Will represent user in the DB

--- first_app/app/models.py
+++ first_app/app/models.py
@@ -3,3 +3,9 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
 
+class User(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
+
+
+
