4f1af109a4934a2f2a26c9761d0fd337bbeb693e
Constantinos <vassalce@gmail.com>
2026-01-29 18:43:11 -0500

    Working on login route

--- first_app/app/routes.py
+++ first_app/app/routes.py
@@ -24,8 +24,15 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
