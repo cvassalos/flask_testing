05d09b03e2d1a81c73a4bd0a119216c779f7ec6f
Constantinos <vassalce@gmail.com>
2026-01-29 18:39:56 -0500

    Added imports to app/routes.py
    Used for flask_login to log users into the db

--- first_app/app/routes.py
+++ first_app/app/routes.py
@@ -1,6 +1,10 @@
 from flask import render_template
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user, login_user
+import sqlalchemy as sa
+from app import db
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
